7d2d4a1f564d7adeb24e74977f0c8cf1ba022a10
Steve Greenberg <spgreenberg@gmail.com>
2018-07-21 20:56:03 -0600

    hystrix for roster

--- src/main/java/io/rscale/training/attendeeviewer/AttendeeViewerApplication.java
+++ src/main/java/io/rscale/training/attendeeviewer/AttendeeViewerApplication.java
@@ -2,12 +2,14 @@ package io.rscale.training.attendeeviewer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class AttendeeViewerApplication {
 
 	public static void main(String[] args) {
